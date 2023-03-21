974ffb36b3c6946dba618087e2c21e37fca08105
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-21 17:18:18 +0100

    [add] declaration

--- declarations.js
+++ declarations.js
@@ -2,17 +2,23 @@ const escapeStr = "`\\/\"'";
 
 const arr = Object.freeze([4, '2']);
 
+const nestedObj = {
+  str: 'Nested object string',
+  num: 10,
+  bool: false
+};
+
+const nestedArr = Object.freeze([4, undefined, '2']);
+
+const nested = Object.freeze({
+  obj: Object.freeze(nestedObj),
+  arr: nestedArr
+});
+
 const obj = Object.freeze({
   str: 'Hello World',
   num: 42,
   bool: true,
   undef: undefined,
-  nested: Object.freeze({
-    arr: Object.freeze([4, undefined, '2']),
-    obj: Object.freeze({
-      str: 'Nested object string',
-      num: 10,
-      bool: false
-    })
-  })
+  nested: nested
 });
