20bd21015df0aa59b796a8cff3669d47a5a0019a
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-22 22:24:27 +0100

    [add] mutability.js

--- mutability.js
+++ mutability.js
@@ -1,5 +1,4 @@
 let person = {
-    name: "Hugo-M Taj",
     age: 25,
     country: "CA",
 };
@@ -15,4 +14,3 @@ console.log("person:", person);
 console.log("clone1:", clone1);
 console.log("clone2:", clone2);
 console.log("samePerson:", samePerson);
-  
