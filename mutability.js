5b90a6f6bdbb4a22e9b2a5e6ea4ff0d403dc93c7
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-22 22:19:14 +0100

    [add] mutability.js

--- mutability.js
+++ mutability.js
@@ -1,18 +1,18 @@
-const person = {
-    name: 'Hugo-M Taj',
-    age: 30,
-    country: 'CA'
-  };
-  
-  const clone1 = Object.assign({}, person);
-  const clone2 = Object.assign({}, person);
-  const samePerson = Object.assign({}, person);
-  
-  person.age += 1;
-  person.country = 'FR';
-  
-  console.log(person);    
-  console.log(clone1);    
-  console.log(clone2);    
-  console.log(samePerson);
+let person = {
+    name: "Hugo-M Taj",
+    age: 25,
+    country: "CA",
+};
+
+let clone1 = Object.assign({}, person);
+let clone2 = Object.assign({}, person);
+let samePerson = person;
+
+person.age += 1;
+person.country = "FR";
+
+console.log("person:", person);
+console.log("clone1:", clone1);
+console.log("clone2:", clone2);
+console.log("samePerson:", samePerson);
   
