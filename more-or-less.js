06a4a3f5613d8a96053f3bca679ab957b33a6691
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-23 18:42:51 +0100

    [add] more or less.js

--- more-or-less.js
+++ more-or-less.js
@@ -1,2 +1,19 @@
-const more = (n) + 1
-const less = (n) - 1
+// Fonction qui ajoute 1 à son argument
+function more(num) {
+    return num + 1;
+}
+
+// Fonction qui soustrait 1 à son argument
+function less(num) {
+    return num - 1;
+}
+
+// Fonction qui additionne deux nombres
+function add(a, b) {
+    return a + b;
+}
+
+// Fonction qui soustrait le deuxième argument du premier
+function sub(a, b) {
+    return a - b;
+}
