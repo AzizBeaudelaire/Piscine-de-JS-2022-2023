0b46b72ef11f5b8d4e9d7136ff37efbdf2e8c7f2
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-24 01:36:07 +0100

    [add] dog years.js

--- dog-years.js
+++ dog-years.js
@@ -1,19 +1,36 @@
-function dogYears(planet, ageInSeconds) {
-    const earthYearInSeconds = 31557600;
-    let dogAgeInYears = ageInSeconds / (earthYearInSeconds * 7);
-    let orbitalPeriods = {
-      earth: 1.0,
-      mercury: 0.2408467,
-      venus: 0.61519726,
-      mars: 1.8808158,
-      jupiter: 11.862615,
-      saturn: 29.447498,
-      uranus: 84.016846,
-      neptune: 164.79132
-    };
-    let orbitalPeriod = orbitalPeriods[planet];
-    let dogAgeOnPlanet = dogAgeInYears / orbitalPeriod;
-    return Math.round(dogAgeOnPlanet * 100) / 100;
-}   
+function dogYears(planet, age) {
+    const earthYears = age / 31557600;
+    let dogYears = 0;
     
-console.log(dogYears('earth', 1000000000)); // Output: 221.82
+    switch(planet) {
+      case 'earth':
+        dogYears = earthYears / 7;
+        break;
+      case 'mercury':
+        dogYears = earthYears / 0.2408467 / 7;
+        break;
+      case 'venus':
+        dogYears = earthYears / 0.61519726 / 7;
+        break;
+      case 'mars':
+        dogYears = earthYears / 1.8808158 / 7;
+        break;
+      case 'jupiter':
+        dogYears = earthYears / 11.862615 / 7;
+        break;
+      case 'saturn':
+        dogYears = earthYears / 29.447498 / 7;
+        break;
+      case 'uranus':
+        dogYears = earthYears / 84.016846 / 7;
+        break;
+      case 'neptune':
+        dogYears = earthYears / 164.79132 / 7;
+        break;
+      default:
+        return 'Invalid planet name.';
+    }
+    
+    return Math.round(dogYears * 100) / 100; // Round to 2 decimal places
+}
+  
