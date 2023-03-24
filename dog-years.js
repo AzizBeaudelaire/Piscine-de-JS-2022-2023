21563fb41bd01b2a55165cdb415441d892e7e7bd
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-24 02:03:39 +0100

    [add] dog years.js

--- dog-years.js
+++ dog-years.js
@@ -1,35 +1,19 @@
-function dogYears(planet, age) {
-    const earthYears = age / 31557600;
-    let dogYears = 0;
-    
-    switch(planet) {
-        case 'earth':
-          dogYears = earthYears / 7;
-          break;
-        case 'mercury':
-          dogYears = earthYears / 0.2408467;
-          break;
-        case 'venus':
-          dogYears = earthYears / 0.61519726;
-          break;
-        case 'mars':
-          dogYears = earthYears / 1.8808158;
-          break;
-        case 'jupiter':
-          dogYears = earthYears / 11.862615;
-          break;
-        case 'saturn':
-          dogYears = earthYears / 29.447498;
-          break;
-        case 'uranus':
-          dogYears = earthYears / 84.016846;
-          break;
-        case 'neptune':
-          dogYears = earthYears / 164.79132;
-          break;
-        default:
-          return 'Invalid planet name.';
-    }  
-    dogYears /= 7; // Convert human years to dog years
-    return (dogYears*100) / 100;
+function dogYears(planet, age_seconds) {
+    const earth_year = 365.25 * 24 * 60 * 60; // seconds in an Earth year
+    const dog_year = earth_year / 7; // seconds in a dog year
+    const orbital_periods = {
+      earth: 1.0,
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132
+    };
+    const orbital_period = orbital_periods[planet];
+    const age_earth_years = age_seconds / earth_year;
+    const age_dog_years = age_earth_years / orbital_period / dog_year;
+    return age_dog_years.toFixed(2);
 }
+  
