afe74ca5483a3fb3ef44cc43bb2404690cce2c44
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-24 01:40:05 +0100

    [add] dog years.js

--- dog-years.js
+++ dog-years.js
@@ -3,34 +3,33 @@ function dogYears(planet, age) {
     let dogYears = 0;
     
     switch(planet) {
-      case 'earth':
-        dogYears = earthYears / 7;
-        break;
-      case 'mercury':
-        dogYears = earthYears / 0.2408467 / 7;
-        break;
-      case 'venus':
-        dogYears = earthYears / 0.61519726 / 7;
-        break;
-      case 'mars':
-        dogYears = earthYears / 1.8808158 / 7;
-        break;
-      case 'jupiter':
-        dogYears = earthYears / 11.862615 / 7;
-        break;
-      case 'saturn':
-        dogYears = earthYears / 29.447498 / 7;
-        break;
-      case 'uranus':
-        dogYears = earthYears / 84.016846 / 7;
-        break;
-      case 'neptune':
-        dogYears = earthYears / 164.79132 / 7;
-        break;
-      default:
-        return 'Invalid planet name.';
-    }
-    
+        case 'earth':
+          dogYears = earthYears / 7;
+          break;
+        case 'mercury':
+          dogYears = earthYears / 0.2408467;
+          break;
+        case 'venus':
+          dogYears = earthYears / 0.61519726;
+          break;
+        case 'mars':
+          dogYears = earthYears / 1.8808158;
+          break;
+        case 'jupiter':
+          dogYears = earthYears / 11.862615;
+          break;
+        case 'saturn':
+          dogYears = earthYears / 29.447498;
+          break;
+        case 'uranus':
+          dogYears = earthYears / 84.016846;
+          break;
+        case 'neptune':
+          dogYears = earthYears / 164.79132;
+          break;
+        default:
+          return 'Invalid planet name.';
+    }  
+    dogYears /= 7; // Convert human years to dog years
     return Math.round(dogYears * 100) / 100; // Round to 2 decimal places
 }
-  
