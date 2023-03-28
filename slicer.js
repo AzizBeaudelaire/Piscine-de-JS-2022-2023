2f1f8a6a2ae03132a17b6d94f936655ce9a558ff
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-28 21:50:09 +0200

    [add] slicer.js

--- slicer.js
+++ slicer.js
@@ -1,9 +1,23 @@
 function slice(input, start, end) {
     if (typeof input === 'string') {
+        if (start < 0) {
+          start = input.length + start;
+        }
+        if (end < 0) {
+          end = input.length + end;
+        }
         return input.substring(start, end);
     } else if (Array.isArray(input)) {
+        if (start < 0) {
+          start = input.length + start;
+        }
+        if (end < 0) {
+          end = input.length + end;
+        }
         return input.slice(start, end);
     } else {
         throw new Error('Input must be a string or an array.');
     }
 }
+
+
