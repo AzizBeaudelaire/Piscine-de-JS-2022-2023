06ba47e890904a7fe5ee723894c38a2a6cfda42a
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-29 00:30:59 +0200

    [add] cut-corners.js

--- cut-corners.js
+++ cut-corners.js
@@ -1,16 +1,20 @@
 function round(number) {
-    return Math.floor(number + 0.5);
+    const decimalPart = number - Math.floor(number);
+    if (decimalPart >= 0.5) {
+        return Math.ceil(number);
+    } else {
+        return Math.floor(number);
+    }
 }
 
 function ceil(number) {
-    return Math.ceil(number);
+    return number === Math.floor(number) ? number : Math.floor(number) + 1;
 }
 
 function floor(number) {
-    return Math.floor(number);
+    return number === Math.ceil(number) ? number : Math.ceil(number) - 1;
 }
 
-  
 function trunc(number) {
-    return number < 0 ? Math.ceil(number) : Math.floor(number);
+    return number > 0 ? Math.floor(number) : Math.ceil(number);
 }
