8a807764cae989d5a1282ee97288ef8c1bf35c62
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-28 21:39:00 +0200

    [add] slicer.js

--- slicer.js
+++ slicer.js
@@ -1,10 +1,9 @@
 function slice(input, start, end) {
-    // Vérifier si l'entrée est une chaîne de caractères ou un tableau
     if (typeof input === 'string') {
-      return input.slice(start, end);
+        return input.substring(start, end);
     } else if (Array.isArray(input)) {
-      return input.slice(start, end);
+        return input.slice(start, end);
     } else {
-      throw new Error('Input must be a string or an array.');
+        throw new Error('Input must be a string or an array.');
     }
 }
