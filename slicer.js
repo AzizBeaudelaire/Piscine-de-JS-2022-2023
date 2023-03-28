11d9ead00712ddd838baaacffda669192f494a4b
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-28 22:47:50 +0200

    [add] slicer.js

--- slicer.js
+++ slicer.js
@@ -1,23 +1,34 @@
-function slice(input, start, end) {
-    if (typeof input === 'string') {
-        if (start < 0) {
-          start = input.length + start;
+function slice(entry, startingPoint = 0, endingPoint = entry.length) {
+    var stringResult = "";
+    var tabResult = [];
+    if (startingPoint < 0) {
+        startingPoint = entry.length + startingPoint; // if sP == -3 for example and entry == 6, sP = 6+(-3) = 3, which is the same result as if it was entry.length - 3 as I did look the commented function bellow
+        if (typeof entry === 'string' && startingPoint < 0) {
+          startingPoint = 0; // adjust negative indices for strings
         }
-        if (end < 0) {
-          end = input.length + end;
+    }
+    if (endingPoint < 0) {
+        endingPoint = entry.length + endingPoint; // negative indices for arrays
+        if (typeof entry === 'string' && endingPoint < 0) {
+          endingPoint = 0; // adjust negative indices for strings
         }
-        return input.substring(start, end);
-    } else if (Array.isArray(input)) {
-        if (start < 0) {
-          start = input.length + start;
+    }
+    for (var i = startingPoint; i <= endingPoint - 1; i++) {
+        if (i >= startingPoint && i < endingPoint) { // between starting and ending points
+            if (typeof entry === 'string') {
+              stringResult += entry[i];
+            } else if (Array.isArray(entry)) {
+              tabResult.push(entry[i])
+            }
         }
-        if (end < 0) {
-          end = input.length + end;
-        }
-        return input.slice(start, end);
+    }
+    if (typeof entry === 'string') {
+        return stringResult;
     } else {
-        throw new Error('Input must be a string or an array.');
+        return tabResult;
     }
 }
 
+
+console.log(slice('abcdef', 2, 4));
 
