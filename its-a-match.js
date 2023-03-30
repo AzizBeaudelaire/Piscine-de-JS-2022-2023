9960c4cd0dec009dfc147d78c5383eb493d6c84c
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-03-30 15:16:05 +0200

    [add] its-a-match.js

--- its-a-match.js
+++ its-a-match.js
@@ -1,7 +1,7 @@
-const normal = "(h)i"; //matches with the expression 'hi'.
+const normal = /hi/; //matches with the expression 'hi'.
 
-const begin = "^(h)i"; //matches with the expression 'hi', only when it is at the beginning.
+const begin = /^hi/; //matches with the expression 'hi', only when it is at the beginning.
 
-const end = "(hi\.)$"; //matches with the expression 'hi', only when it is at the end.
+const end = /hi$/; //matches with the expression 'hi', only when it is at the end.
 
-const beginEnd = "^(hi\.)$"; //matches with the expression 'hi', only when it is exactly hi.
+const beginEnd = /^hi$/; //matches with the expression 'hi', only when it is exactly hi.
