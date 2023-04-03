4c6e2edf603b343602f6e099292333d2b54e331c
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-03 15:01:19 +0200

    [add] vowel.js

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,13 +1,7 @@
-function vowelDots(s) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    let result = '';
-    for (let i = 0; i < s.length; i++) {
-        if (vowels.indexOf(s[i].toLowerCase()) !== -1) {
-            result += s[i] + '.';
-        } else {
-            result += s[i];
-        }
-    }
-    return result;
-  }
-  
+const vowels = /[aeiou]/gi;
+
+const vowelDots = (str) =>{
+    return str.replace(vowels,'$&.');
+}
+
+console.log(vowelDots('Salut les gens'));
