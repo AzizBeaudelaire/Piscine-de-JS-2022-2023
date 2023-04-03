012f31bf174cb816eb709f7e7612445f74ddd4e3
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-03 15:34:48 +0200

    [add] same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,17 +1,24 @@
 function sameAmount(str, regex1, regex2) {
     let count1 = 0;
     let count2 = 0;
-    let match1;
-    let match2;
   
-    while ((match1 = regex1.exec(str)) !== null) {
-        count1++;
+    while (true) {
+        let match1 = regex1.exec(str);
+        let match2 = regex2.exec(str);
+  
+        if (!match1 && !match2) {
+            return count1 === count2;
+        }
+  
+        if (match1) {
+            count1++;
+        }
+  
+        if (match2) {
+            count2++;
+        }
     }
-  
-    while ((match2 = regex2.exec(str)) !== null) {
-        count2++;
-    }
-  
-    return count1 === count2;
 }
+  
+  
   
