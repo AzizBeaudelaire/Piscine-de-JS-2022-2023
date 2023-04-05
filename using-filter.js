63bf7a1039d56f9372d4b768e56f133ff805d496
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-05 21:55:28 +0200

    [add] using-filter.js

--- using-filter.js
+++ using-filter.js
@@ -1,37 +1,40 @@
+// filterShortStateName
 function filterShortStateName(arr) {
-    return arr.filter(str => str.length < 7);
+    return arr.filter(state => state.length < 7);
 }
 
+// filterStartVowel
 function filterStartVowel(arr) {
-    return arr.filter(str => ['a', 'e', 'i', 'o', 'u'].includes(str[0].toLowerCase()));
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return arr.filter(state => vowels.includes(state.toLowerCase()[0]));
 }
 
+// filter5Vowels
 function filter5Vowels(arr) {
-    return arr.filter(str => {
-        const vowels = str.match(/[aeiou]/gi);
-    return vowels && vowels.length >= 5;
-    });
+    const regex = /[aeiou]/gi;
+    return arr.filter(state => state.match(regex)?.length >= 5);
 }
 
+// filter1DistinctVowel
 function filter1DistinctVowel(arr) {
-    return arr.filter(str => {
-        const vowels = str.match(/[aeiou]/gi);
-    return vowels && new Set(vowels).size === 1;
-    });
+    return arr.filter(state => {
+    const vowels = new Set(state.toLowerCase().match(/[aeiou]/g));
+    return vowels.size === 1;
+  });
 }
 
+// multiFilter
 function multiFilter(arr) {
     return arr.filter(obj => {
-        const capital = obj.capital;
-        const name = obj.name;
-        const tag = obj.tag;
-        const region = obj.region;
-
-    return capital.length >= 8
-        && !['a', 'e', 'i', 'o', 'u'].includes(name[0].toLowerCase())
-        && tag.match(/[aeiou]/i)
-        && region !== 'South';
+    const { capital, name, tag, region } = obj;
+        const vowels = /[aeiou]/i;
+        return (
+            capital.length >= 8 &&
+            !vowels.test(name[0]) &&
+            tag.match(vowels) &&
+            region !== 'South'
+        );
     });
 }
-      
+
   
