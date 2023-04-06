bd702f6f651a0575fa581bc51daed96e1891695d
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-06 23:31:18 +0200

    [add] mapper.js

--- mapper.js
+++ mapper.js
@@ -1,7 +1,7 @@
 function map(arr, func) {
     const result = [];
     for (let i = 0; i < arr.length; i++) {
-        result.push(func(arr[i], i, arr));
+        result.push(func(arr[i]));
     }
     return result;
 }
@@ -9,11 +9,14 @@ function map(arr, func) {
 function flatMap(arr, func) {
     const result = [];
     for (let i = 0; i < arr.length; i++) {
-        const mapped = func(arr[i], i, arr);
-        result.push(...mapped);
+        const mapped = func(arr[i]);
+        for (let j = 0; j < mapped.length; j++) {
+            result.push(mapped[j]);
+        }
     }
     return result;
 }
+  
 
 /*Instructions
 Create a map function that takes an array as the first argument, a function as 
