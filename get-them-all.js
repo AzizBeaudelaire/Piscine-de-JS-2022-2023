fb669ea042a2d59fe29a8f7564a96974ae3a8523
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-10 20:52:35 +0200

    [add] get-them-all.js

--- get-them-all.js
+++ get-them-all.js
@@ -1,76 +1,38 @@
-function getArchitects() {
-    const architects = [];
-    const nonArchitects = [];
-    const people = document.querySelectorAll('#peopleList > li');
-    people.forEach(person => {
-        const isArchitect = person.classList.contains('architect');
-        if (isArchitect) {
-            architects.push(person.querySelector('a'));
-        } else {
-            nonArchitects.push(person);
-        }
-    });
-    return [architects, nonArchitects];
+export const getArchitects = () => {
+    const architects = Array.from(document.getElementsByTagName('a'))
+        .filter(a => a.textContent === 'Architect')
+  
+    const nonArchitects = Array.from(document.getElementsByTagName('span'))
+        .filter(span => !span.classList.contains('name') || span.classList.contains('Architect'))
+  
+    return [architects, nonArchitects]
 }
   
-function getClassical() {
-    const classicalArchitects = [];
-    const nonClassicalArchitects = [];
-    const people = document.querySelectorAll('#peopleList > li');
-    people.forEach(person => {
-        const isClassical = person.classList.contains('classical');
-        const isArchitect = person.classList.contains('architect');
-        if (isArchitect) {
-            if (isClassical) {
-                classicalArchitects.push(person.querySelector('a'));
-            } else {
-                nonClassicalArchitects.push(person.querySelector('a'));
-            }
-        }
-    });
-    return [classicalArchitects, nonClassicalArchitects];
+export const getClassical = () => {
+    const classicalArchitects = Array.from(document.getElementsByClassName('classical'))
+        .filter(a => a.textContent.includes('Architect'))
+  
+    const nonClassicalArchitects = Array.from(document.getElementsByTagName('a'))
+        .filter(a => !a.classList.contains('classical') || !a.textContent.includes('Architect'))
+  
+    return [classicalArchitects, nonClassicalArchitects]
 }
   
-function getActive() {
-    const activeClassicalArchitects = [];
-    const nonActiveClassicalArchitects = [];
-    const people = document.querySelectorAll('#peopleList > li');
-    people.forEach(person => {
-        const isClassical = person.classList.contains('classical');
-        const isArchitect = person.classList.contains('architect');
-        const isActive = person.classList.contains('active');
-        if (isArchitect) {
-            if (isClassical) {
-                if (isActive) {
-                    activeClassicalArchitects.push(person.querySelector('a'));
-                } else {
-                    nonActiveClassicalArchitects.push(person.querySelector('a'));
-                }
-            } else {
-                nonActiveClassicalArchitects.push(person.querySelector('a'));
-            }
-        }
-    });
-    return [activeClassicalArchitects, nonActiveClassicalArchitects];
+export const getActive = () => {
+    const classicalArchitects = Array.from(document.getElementsByClassName('classical'))
+        .filter(a => a.textContent.includes('Architect'))
+  
+    const activeArchitects = classicalArchitects.filter(a => !a.classList.contains('inactive'))
+    const nonActiveArchitects = classicalArchitects.filter(a => a.classList.contains('inactive'))
+  
+    return [activeArchitects, nonActiveArchitects]
 }
   
-function getBonannoPisano() {
-    const activeClassicalArchitects = [];
-    const bonannoPisano = document.querySelector('#BonannoPisano');
-    const people = document.querySelectorAll('#peopleList > li');
-    people.forEach(person => {
-        const isClassical = person.classList.contains('classical');
-        const isArchitect = person.classList.contains('architect');
-        const isActive = person.classList.contains('active');
-        if (isArchitect && isClassical) {
-            if (person.querySelector('a') === bonannoPisano) {
-                return;
-            }
-            if (isActive) {
-                activeClassicalArchitects.push(person.querySelector('a'));
-            }
-        }
-    });
-    return [bonannoPisano, activeClassicalArchitects];
+export const getBonannoPisano = () => {
+    const bonannoPisano = document.getElementById('BonannoPisano')
+    const activeClassicalArchitects = Array.from(document.querySelectorAll('.classical:not(.inactive)'))
+        .filter(a => a.id !== 'BonannoPisano')
+  
+    return [bonannoPisano, activeClassicalArchitects]
 }
   
