bc47494caeb11f39211942aac9b7e184fe7a220a
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-12 18:26:42 +0200

    [add] build-brick-and-break.js

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,40 +1,57 @@
-let brickCount = 0;
-
-function build(brickAmount) {
-    const tower = document.querySelector('.tower');
-    const middleColumn = document.querySelector('.column.middle');
+// build function
+function build(amount) {
+    let id = 1
+    const columns = document.querySelectorAll('.column')
   
-    for (let i = 1; i <= brickAmount; i++) {
-        const brick = document.createElement('div');
-        brick.classList.add('brick');
-        brick.id = `brick-${i}`;
-        brickCount++;
-        if (i % 3 === 2) {
-            brick.dataset.foundation = true;
-            middleColumn.appendChild(brick);
-        } else {
-            tower.appendChild(brick);
+    // create brick div and add to column at regular interval
+    const interval = setInterval(() => {
+        if (id > amount) {
+            clearInterval(interval)
+            return
         }
-    setTimeout(() => brick.classList.add('built'), i * 100);
-    }
+        const brick = document.createElement('div')
+        brick.id = `brick-${id}`
+        brick.classList.add('brick')
+        if (id % 3 === 2) {
+            brick.setAttribute('data-foundation', true)
+        }
+        columns[Math.floor(Math.random() * 3)].appendChild(brick)
+        id++
+    }, 100)
 }
-
+  
+  // repair function
 function repair(...ids) {
-    for (const id of ids) {
-        const brick = document.getElementById(id);
-        if (brick.dataset.foundation === 'true') {
-            brick.dataset.repaired = 'in progress';
+    ids.forEach(id => {
+        const brick = document.getElementById(id)
+        if (brick.parentNode.classList.contains('middle')) {
+            brick.setAttribute('data-repaired', 'in progress')
         } else {
-            brick.dataset.repaired = true;
+            brick.setAttribute('data-repaired', true)
         }
-    }
+    })
 }
-
+  
+  // destroy function
 function destroy() {
-    const tower = document.querySelector('.tower');
-    const lastBrick = tower.lastChild;
-    if (lastBrick) {
-        lastBrick.remove();
-        brickCount--;
+    const bricks = document.querySelectorAll('.brick')
+    if (bricks.length > 0) {
+        const lastBrick = bricks[bricks.length - 1]
+        lastBrick.parentNode.removeChild(lastBrick)
     }
 }
+  
+  // click handlers
+document.querySelector('.hammer').addEventListener('click', () => {
+    const ids = document.querySelectorAll('.brick[data-repaired="false"], .brick[data-repaired="in progress"]')
+    repair(...Array.from(ids).map(brick => brick.id))
+})
+  
+document.querySelector('.bomb').addEventListener('click', () => {
+    destroy()
+})
+  
+  // build 10 bricks
+build(10);
+  
+  
