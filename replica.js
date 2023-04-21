b1e654661aec92dc66b29fd83e7e1ee5bbc86b1d
HugoMehdi Taj <hugomehdi.taj@ynov.com>
2023-04-21 19:49:52 +0200

    [add] replica.js

---
+++ replica.js
@@ -0,0 +1,32 @@
+var is = {};
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) =>
+    typeof n === 'object' &&
+    !is.fun(n) &&
+    !is.arr(n) &&
+    n !== null &&
+    !(n instanceof RegExp);
+is.fun = (n) => typeof n === 'function';
+
+function replica(target, ...sources) {
+    sources.forEach((source) => {
+        Object.keys(source).forEach((key) => {
+            if (is.obj(source[key])) {
+                if (!target.hasOwnProperty(key) || !is.obj(target[key])) {
+                    target[key] = {};
+                }
+                replica(target[key], source[key]);
+            } else {
+                target[key] = source[key];
+            }
+        });
+    });
+    return target;
+}
+
+/*Instructions
+Create a function named replica that allows you to deep assign the values of all properties from one or more objects to a target object.
+
+Watch out for shallow copies.
+
+*/
